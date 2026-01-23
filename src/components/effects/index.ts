874dfbe5f9f2553e8fc4d1738b63085a14337db5
adrioui <adrifdlh@gmail.com>
2026-01-24 00:34:43 +0700

    feat(effects): add barrel exports

---
+++ src/components/effects/index.ts
@@ -0,0 +1,6 @@
+export { Confetti } from './Confetti';
+export { GlitchOverlay } from './GlitchOverlay';
+export { ScreenShake } from './ScreenShake';
+export { SystemReboot } from './SystemReboot';
+export { Sticker } from './Sticker';
+export { Badge } from './Badge';
