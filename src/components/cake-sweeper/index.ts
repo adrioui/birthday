df3368c3ac011303a805678f0065aa55c9b02af2
adrioui <adrifdlh@gmail.com>
2026-01-24 00:32:51 +0700

    feat(cake-sweeper): add barrel export

---
+++ src/components/cake-sweeper/index.ts
@@ -0,0 +1,4 @@
+export { CakeSweeperGrid } from './CakeSweeperGrid';
+export { CakeSweeperTile } from './CakeSweeperTile';
+export { CounterDisplay } from './CounterDisplay';
+export { Win95Window } from './Win95Window';
